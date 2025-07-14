146a42bc7531349aa9c99845936443c42f643dc4
kabir5145 <kabir10112006@gmail.com>
2025-07-14 16:21:31 +0530

    practice 2D arrays  in java

--- com/util/company/practice_2d_arrays.java
+++ com/util/company/practice_2d_arrays.java
@@ -4,24 +4,41 @@ import java.util.Scanner;
 
 public class practice_2d_arrays {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Insert the numbers of rows :");
         int a = sc.nextInt();
+        System.out.println("Insert the numbers of columns: ");
         int b = sc.nextInt();
         int[][] number = new int[a][b];
+        int num = 11;
 
         //input
-        for (int i = 0;i<a;i++){
-            for(int k = 0;k<b;k++) {
-                number[i][k]= sc.nextInt();
+        for (int i = 0; i < a; i++) {
+            for (int k = 0; k < b; k++) {
+                number[i][k] = sc.nextInt();
             }
-
-            //output
-            for (i = 0; i<a; i++) {
-                for (int k = 0; k < b; k++) {
-                    System.out.print(number[i][k] +" ");
+        }
+        //output
+        for (int i = 0; i < a; i++) {
+            for (int k = 0; k < b; k++) {
+                System.out.print(number[i][k] + "  ");
+            }
+            System.out.println();
+        }
+        boolean found=false;
+        for(int i = 0;i< a ;i++) {
+            for (int k = 0; k < b; k++) {
+                if (number[i][k] == num) {
+                    found = true;
+                    break;
                 }
-                System.out.println();
+            }
+        }
+            if (found) {
+                System.out.println("Found the given number in the matrix");
+            } else {
+                System.out.println("sorry ! Not found the number in the given matrix");
             }
         }
     }
-}
+
