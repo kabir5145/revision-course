0cf4902deaf27a07afad6e64feee15272cf637b6
kabir5145 <kabir10112006@gmail.com>
2025-07-18 14:30:53 +0530

    practicing java

--- com/company/String_12.java
+++ com/company/String_12.java
@@ -14,15 +14,47 @@ public class String_12 {
 //        System.out.println("Your name is : "+name);
 
         //concatenation
-        String firstName = "kabir";
-        String lastName = "Singh";
-        System.out.println(firstName+" "+lastName);
-        String fullName = firstName+lastName;
-        System.out.println(fullName.length());
+//        String firstName = "kabir";
+//        String lastName = "Singh";
+//        System.out.println(firstName+" "+lastName);
+//        String fullName = firstName+lastName;
+//        System.out.println(fullName.length());
+//
+//        //charAt
+//        for(int i = 0;i<fullName.length();i++) {
+//            System.out.print(fullName.charAt(i));
+//        }
 
-        //charAt
-        for(int i = 0;i<fullName.length();i++) {
-            System.out.print(fullName.charAt(i));
+        String a1 = "kabir";
+        String a2 = "kabir";
+
+        //1 s1>s2: +ve value
+        //2 s1 == s2: 0
+        //3 s1<s2: -ve value
+        /*
+        if(a1.compareTo(a2)==0){
+            System.out.println("This is correct !");
+        }else {
+            System.out.println("String are not equal");
         }
+         */
+//        if(a1==a2){
+//            System.out.println("This is correct !");
+//        }else {
+//            System.out.println("String are not equal");
+//        }
+//        if(new String("hello")==new String("hello")){
+//            System.out.println("This is correct !");
+//        }else {
+//            System.out.println("String are not equal");
+//        }
+
+        //finding between the string
+       // String sentence = "my name is tony , and the match";
+        String sentence = "tonyStark";
+        String name = sentence.substring(0,4);
+        System.out.println(name);
+
+
     }
 }
