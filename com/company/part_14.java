2703285e02fabea3ce733defc394b84f0c5f75a7
kabir5145 <kabir10112006@gmail.com>
2025-08-11 22:20:14 +0530

    pre- and post-decrement as well as increment  in java

---
+++ com/company/part_14.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class part_14 {
+    public static void main(String[] args) {
+      //pre Increment
+        int a = 10;
+        int b = 0;
+        b = a++;
+        System.out.println(a);
+        System.out.println(b);
+
+        //pre Decrement
+        int e = 10;
+        int f = 0;
+        f = --e;
+        System.out.println(e);
+        System.out.println(f);
+        //post Increment
+        int c = 10;
+        int d = 0;
+        d= ++c;
+        System.out.println(c);
+        System.out.println(d);
+
+        //post Decrement
+        int g = 10;
+        int h = 0;
+        h = g--;
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
