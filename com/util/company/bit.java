1667b7cabb8d9e2a0181c4b6e14f9c1d8dcea1f8
kabir5145 <kabir10112006@gmail.com>
2025-08-14 20:49:48 +0530

    Bitwise  operators  in java

---
+++ com/util/company/bit.java
@@ -0,0 +1,10 @@
+package com.util.company;
+
+public class bit {
+    public static void main(String[] args) {
+        int a = 0101;
+        int b = 0110;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
