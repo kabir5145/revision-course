55deba7d003433a3b3afca3f82a82d56e57bab4e
kabir5145 <kabir10112006@gmail.com>
2025-08-19 01:27:34 +0530

    question 2 of  Bit Manipulation  in java

--- com/util/company/bit_practice.java
+++ com/util/company/bit_practice.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class bit_practice {
     public static void main(String[] args) {
         //question  1: if a number is odd or even
+        /*
         Scanner sc = new Scanner(System.in);
         System.out.println("Insert the number:");
         int a = sc.nextInt();
@@ -13,5 +14,12 @@ public class bit_practice {
         }else {
             System.out.println("Odd");
         }
+         */
+
+        //question 2: Get the ith bit of a number
+        int n = 13;//1101
+        int i = 2;
+        int bitMask = (n>>i)&1;
+        System.out.println(bitMask);
     }
 }
