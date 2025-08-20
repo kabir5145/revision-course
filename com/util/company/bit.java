f5ff0ab19b41a5c75f4b12b5c2a47b1c3617c7fe
kabir5145 <kabir10112006@gmail.com>
2025-08-20 22:00:55 +0530

    Bit Manipulation  in java

--- com/util/company/bit.java
+++ com/util/company/bit.java
@@ -2,9 +2,11 @@ package com.util.company;
 
 public class bit {
     public static void main(String[] args) {
-        int a = 0101;
-        int b = 0110;
-        System.out.println(a);
-        System.out.println(b);
+        int a = 5;//0101
+        int b = 3;//0011
+        int result= a&b;//0001(decimal1)
+        int r = a|b;
+        System.out.println(result);
+        System.out.println(r);
     }
 }
