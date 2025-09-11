361af2b8f8e7a03f6dd694d338fd1eab012feb27
kabir5145 <kabir10112006@gmail.com>
2025-09-11 16:32:04 +0530

    revising Inheritance in OOPS in java

--- com/company/OOPs.java
+++ com/company/OOPs.java
@@ -1,30 +1,32 @@
 package com.company;
 
-//second example
-class Student {
-    String name;
-    int age;
+//Inheritance in java
 
-    //Function overloading
-    public void printInfo(String name) {
-        System.out.println(name);
-    }
-    public void printInfo(int age){
-        System.out.println(age);
-    }
-    public void printInfo(String name,int age){
-        System.out.println(name+" "+age);
+class Shape{
+    public void area(){
+        System.out.println("Displays area");
     }
 }
 
+class Triangle extends Shape {
+    public void area(int l,int h){
+        System.out.println((1 / 2) * l * h);
+    }
+}
+
+class Circle extends Shape {
+    public void area(int r){
+        System.out.println(Math.PI*r*r);
+    }
+}
 public class OOPs {
     public static void main(String[] args) {
+        Triangle t = new Triangle();
+        System.out.println("Area of triangle is : ");
+        t.area(2,1);
 
-        Student s_1 = new Student();
-        s_1.name= "kabir";
-        s_1.age = 18;
-
-        s_1.printInfo(s_1.name,s_1.age);
-
+        Circle c = new Circle();
+        System.out.println("Area of circle is : ");
+        c.area(5);
     }
 }
